a2c8c11aaf1a796ccabc5e000884b4f19c8304ae
Zak Golding <hello@zakarhino.com>
2016-02-11 22:23:13 -0800

    Updated application

--- client/app/application.js
+++ client/app/application.js
@@ -1,6 +1,17 @@
 angular.module('forinlanguages', [
+  'ngRoute',
   'forinlanguages.services',
   'forinlanguages.peer'
 ])
 
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app/views/main.html',
+      controller: 'PeerController',
+    })
+    .otherwise({
+      redirect: '/'
+    })
+})
 // Main app stuff here
