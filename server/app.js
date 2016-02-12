f05185c75519c2be9d6e40333495e80b09878954
john chau <johnchau@johns-MacBook-Pro.local>
2016-02-11 16:22:39 -0800

    Express listening on port 3000

--- server/app.js
+++ server/app.js
@@ -1,25 +1,39 @@
-var http = require('http');
+// var http = require('http');
 
-var nStatic = require('node-static');
+// var nStatic = require('node-static');
 
-var fileServer = new nStatic.Server('./public');
+// var fileServer = new nStatic.Server('./public');
 
-http.createServer(function (req, res) {
+// http.createServer(function (req, res) {
     
-    fileServer.serve(req, res);
+//     fileServer.serve(req, res);
 
-}).listen(5000);
-
-
+// }).listen(5000);
 
 
 
-// var express = require('express');
-// var app = express();
 
-// app.use(express.static('public'));
 
-// //Serves all the request which includes /images in the url from Images folder
-// app.use('/images', express.static(__dirname + '/Images'));
+var express = require('express');
+var bodyParser = require('body-parser');
 
-// var server = app.listen(5000);
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use('views', __dirname + '/client');
+var port = process.env.PORT || 3000
+
+app.use(express.static(__dirname+'/client'));
+
+app.get('/', function (req, res){
+	//res.render('index');
+	res.send('hello')
+})
+//Serves all the request which includes /images in the url from Images folder
+//app.use('/images', express.static(__dirname + '/Images'));
+
+var server = app.listen(port);
+
+console.log('Sever is now listening on port '+ port);
