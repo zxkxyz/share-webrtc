48e8afdaeafa6d56699932b3c1e226f0c888676a
Joseph Ashby <jashby473@gmail.com>
2016-02-17 18:29:07 -0800

    rebase commit

--- client/app/application.js
+++ client/app/application.js
@@ -4,6 +4,18 @@ angular.module('forinlanguages', [
   'forinlanguages.services',
   'forinlanguages.peer'
 ])
+<<<<<<< a5d4e2568a05019467fa07cc6e93964689c617d1
 .config(['$localForageProvider', function($localForageProvider){
   $localForageProvider.setNotify(true, true); // itemSet, itemRemove
 }]);
+=======
+
+// .config(function($routeProvider,$httpProvider){
+//     $routeProvider
+//     .when('/', {
+//         templateUrl: 'index.html',
+//         controller : 'PeerController'
+//     })
+// })
+// Main app stuff here
+>>>>>>> rebase commit
