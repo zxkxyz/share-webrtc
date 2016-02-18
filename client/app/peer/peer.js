a5d4e2568a05019467fa07cc6e93964689c617d1
Joseph Ashby <jashby473@gmail.com>
2016-02-17 16:04:28 -0800

    added enter key listener

--- client/app/peer/peer.js
+++ client/app/peer/peer.js
@@ -165,7 +165,12 @@ angular.module('forinlanguages.peer', [])
       alert("you screwed up");
     }
   };
-
+  $scope.isEnter = function(envent, func, arg){
+    console.log("listening to keys");
+    if(envent.keyCode===13){
+      func.apply(null, arg);
+    }
+  }
   $scope.destroyPeer = function() {
     console.log("destroyed func!");
     console.log('before', $scope.me); 
