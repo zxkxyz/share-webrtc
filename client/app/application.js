6ea9fdb46dcc2ef208b1f676c0237a641fb4641c
Joseph Ashby <jashby473@gmail.com>
2016-02-18 15:02:05 -0800

    Update application.js
    got rid of git rebase leftovers

--- client/app/application.js
+++ client/app/application.js
@@ -4,11 +4,8 @@ angular.module('forinlanguages', [
   'forinlanguages.services',
   'forinlanguages.peer'
 ])
-<<<<<<< a5d4e2568a05019467fa07cc6e93964689c617d1
-.config(['$localForageProvider', function($localForageProvider){
-  $localForageProvider.setNotify(true, true); // itemSet, itemRemove
-}]);
-=======
+
+
 
 // .config(function($routeProvider,$httpProvider){
 //     $routeProvider
@@ -18,4 +15,5 @@ angular.module('forinlanguages', [
 //     })
 // })
 // Main app stuff here
->>>>>>> rebase commit
+
+
